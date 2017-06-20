beb53d900c2f28b5a51c11082b41de03d0baf83f
Lanna <Lanna_lmb@hotmail.com>
2017-06-20 18:21:47 -0400

    Initial Commit

---
+++ HelloWorldAssessment/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldAssessment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Project Overview";
+
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Lanna Brasure - Computer Software Engineer";
+
+            return View();
+        }
+
+
+    }
+}
---
+++ HelloWorldAssessment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloWorldAssessment.MvcApplication" Language="C#" %>
